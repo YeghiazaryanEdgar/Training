39cfa3f8823fdf22bd096aca9dd1c90ecba32174
YeghiazaryanEdgar <melfox701Gmail.com>
2018-08-20 15:35:55 +0400

    SWAP_WITHOUT_C

---
+++ Tasks/C++/Swap_Without_C/swap_without_c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+
+    double a = 10;
+    double b = 20;
+
+    a = a + b;  // 30
+    b = a - b;  // 10
+    a = a - b;  // 20
+
+
+    return 0;
+}
+
+
+
+
+
