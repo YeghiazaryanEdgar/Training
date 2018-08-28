1e7a9dba688ba59bc486100de4a78ff39c7bfb88
YeghiazaryanEdgar <melfox701Gmail.com>
2018-08-28 15:20:35 +0400

    MODIFY: swap_variables_values

--- Tasks/C++/Swap_Variables_Values/swap_variables_values.cpp
+++ Tasks/C++/Swap_Variables_Values/swap_variables_values.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 using namespace std;
 
-
+void swapValues(double &a, double &b) {
+   a = a + b;
+   b = a - b;
+   a = a - b;
+}
 
 int main() {
 
     double a = 10;
     double b = 20;
 
-    a = a + b;  // 30
-    b = a - b;  // 10
-    a = a - b;  // 20
+    swapValues(a, b);
 
+    cout<<"a: "<< a <<endl;
+    cout<<"b: "<< b <<endl;     
 
     return 0;
 }
