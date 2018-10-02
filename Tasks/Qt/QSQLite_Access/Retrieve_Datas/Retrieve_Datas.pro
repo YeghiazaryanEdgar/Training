d16c8076a1ac0a239b22db02b6aca0c303457355
YeghiazaryanEdgar <melfox701gmail.com>
2018-10-02 12:59:58 +0400

    ADD: QSQLite_Access

---
+++ Tasks/Qt/QSQLite_Access/Retrieve_Datas/Retrieve_Datas.pro
@@ -0,0 +1,7 @@
+SOURCES += \
+    main.cpp
+
+QT       += core sql
+
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
---
+++ Tasks/Qt/QSQLite_Access/Retrieve_Datas/main.cpp
@@ -0,0 +1,48 @@
+#include <QApplication>
+#include <QFile>
+#include <QtSql>
+#include <QtDebug>
+
+int main(int argc, char *argv[]) {
+
+      if(argc != 3) {
+         qDebug() << "Wrong number of arguments";
+         return 1;
+      }
+      QString db_PATH = argv[1];
+      QString db_name = argv[2];
+
+
+      if(db_name.mid(db_name.length() - 3, 3) != ".db") {
+          db_name += ".db";
+      }
+
+      if(QFile::exists(db_PATH + "/" + db_name)) {
+
+                    QSqlDatabase mydb = QSqlDatabase::addDatabase("QSQLITE");
+                    mydb.setDatabaseName(db_PATH + "/" + db_name);
+
+                    if(!mydb.open()) {
+                        qDebug("Database Open Failed!");
+                    } else {
+                        QSqlQuery query("SELECT * FROM mytable");
+
+                        if(query.exec()) {
+                            while(query.next()) {
+                                qDebug().noquote() << "| ID "         <<"|" << query.value(0).toInt();
+                                qDebug().noquote() << "| NAME "       <<"|" << query.value(1).toString();
+                                qDebug().noquote() << "| TOOLCHAINS " <<"|" << query.value(2).toString();
+                                qDebug().noquote() << "| FAMILIES "   <<"|" << query.value(3).toString() << "\n";
+                            }
+                        } else {
+                            qDebug() << query.lastError().text();
+                        }
+                    }
+       } else {
+            qDebug() << "Invalid Path/Database name";
+       }
+
+
+
+    return 0;
+}
